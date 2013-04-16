fd79b5c64235811e2075a3d36cd40523722e5c6c
zwt <zwtlong@gmail.com>
2013-04-16 13:24:39 +0800

    Teset2

---
+++ src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+	
+	
+	public void test(){}
+	
+	public void fesh(){
+		System.out.println("---");
+		System.out.println("---22");
+	}
+	
+}
